00ed2a0ec1e636e083d15f22aeb1dc0a95fb9696
gr8manbir <manbir.129@gmail.com>
2017-10-28 12:39:06 -0400

    Updating model to use ELU instead of RELU

--- Self_Driving_Car/CarND-Behavioral-Cloning-P3/model.py
+++ Self_Driving_Car/CarND-Behavioral-Cloning-P3/model.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 from keras.models import Sequential, model_from_json
-from keras.layers import Flatten, Dense, Dropout, Activation, Lambda, Cropping2D, Convolution2D 
+from keras.layers import Flatten, Dense, Dropout, Activation, Lambda, Cropping2D, Convolution2D,ELU
 from scipy.misc import imread
 
 path_data='./Data'
@@ -22,19 +22,26 @@ def NVidiaModel():
     model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(3,160,320)))
     
     #Add convn layers
-    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode="same", activation="relu"))
-    model.add(Convolution2D(36, 5, 5, subsample = (2, 2), border_mode="same", activation="relu"))
-    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode="same", activation="relu"))
-    model.add(Convolution2D(64, 3, 3 , border_mode='same', activation='relu'))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode="same"))
+    model.add(ELU())
+    model.add(Convolution2D(36, 5, 5, subsample = (2, 2), border_mode="same"))
+    model.add(ELU())
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode="same")
+    model.add(ELU())
+    model.add(Convolution2D(64, 3, 3 , border_mode='same')
+    model.add(ELU())
     model.add(Convolution2D(64, 3, 3 , border_mode='same')) # Manbir - check if activation makes a difference here
-    
+    model.add(ELU())
     #Add fully connected layers
     model.add(Flatten())
-    model.add(Dense(100, activation='relu'))
+    model.add(Dense(100)
     model.add(Dropout(0.5))
-    model.add(Dense(50, activation='relu'))
+    model.add(ELU())
+    model.add(Dense(50))
     model.add(Dropout(0.5))
+    model.add(ELU())
     model.add(Dense(10))
+    model.add(ELU())
     model.add(Dense(1)) # We want only one steering angle based on input images
     return model
 
