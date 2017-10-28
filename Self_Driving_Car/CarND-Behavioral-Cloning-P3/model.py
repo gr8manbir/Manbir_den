92e6b6f69927a1ff233056b5aaf87aaea9ada256
gr8manbir <manbir.129@gmail.com>
2017-10-28 15:54:07 -0400

    Updating model

--- Self_Driving_Car/CarND-Behavioral-Cloning-P3/model.py
+++ Self_Driving_Car/CarND-Behavioral-Cloning-P3/model.py
@@ -19,7 +19,7 @@ def NVidiaModel():
     #Normalize the image
     model.add(Lambda(lambda x: (x/127.5)-1.0, input_shape=(160,320,3)))
     #Crop the images
-    model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(3,160,320)))
+    model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
     
     #Add convn layers
     model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode="valid"))
