bbda92bc28b0b95352e16f91b59a4939f0157a80
gr8manbir <manbir.129@gmail.com>
2018-01-28 14:48:57 -0500

    Updating jacobian calculation

--- Self_Driving_Car/CarND-Extended-Kalman-Filter-Project/src/tools.cpp
+++ Self_Driving_Car/CarND-Extended-Kalman-Filter-Project/src/tools.cpp
@@ -61,16 +61,17 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   float vx = x_state(2);
   float vy = x_state(3);
 
+  //Avoid zero in dr values
+  if( fabs(px) < 0.001 ) px = 0.001;
+  if( fabs(py) < 0.001 ) py = 0.001;
+  
   //pre-compute a set of terms to avoid repeated calculation
   float c1 = px*px+py*py;
+  
+  //c1 should not be zero
+  if( fabs(c1) < 0.001 ) c1 = 0.001;
   float c2 = sqrt(c1);
   float c3 = (c1*c2);
-
-  //check division by zero
-  if(fabs(c1) < 0.0001){
-	cout << "CalculateJacobian () - Error - Division by Zero" << endl;
-	return Hj;
-  }
 
   //compute the Jacobian matrix
   Hj << (px/c2), (py/c2), 0, 0,
