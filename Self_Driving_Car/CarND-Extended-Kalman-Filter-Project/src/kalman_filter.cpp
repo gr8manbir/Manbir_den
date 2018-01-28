9ecf1379260e603e3527f270a0634e8f17b6aecb
gr8manbir <manbir.129@gmail.com>
2018-01-28 18:09:39 -0500

    Proper normalization procedure

--- Self_Driving_Car/CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
+++ Self_Driving_Car/CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
@@ -76,10 +76,13 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   //Use equations from class
   VectorXd y = z - hx;
   //Normalize angle i.e. y(1)
-  double width = 2 * PI;   //
+  
+  //Normalization: https://stackoverflow.com/questions/24234609/standard-way-to-normalize-an-angle-to-%CF%80-radians-in-java
+  /*double width = 2 * PI;   //
   double offsetValue = y(1) + PI;   // value relative to 0
   y(1) = (offsetValue - (floor(offsetValue / width) * width)) - M_PI;
-  
+  */
+  y(1) = y(1) - (2*PI*floor((y(1)+PI)/2*PI))
   MatrixXd Ht = H_.transpose();
   MatrixXd S = H_ * P_ * Ht + R_;
   MatrixXd Si = S.inverse();
