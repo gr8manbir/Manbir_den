603896488c8a25d9e70fcbebce70cf3a51c2ca1d
gr8manbir <manbir.129@gmail.com>
2018-01-28 15:13:05 -0500

    correcting atan2

--- Self_Driving_Car/CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
+++ Self_Driving_Car/CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
@@ -67,7 +67,7 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   float vy = x_[3];
 
   float rho = sqrt(px*px + py*py);
-  float phi = atan2(py/px); 
+  float phi = atan2(py,px); 
   float rho_dot = (px*vx+py*vy)/rho;
 
   VectorXd hx = VectorXd(3);
