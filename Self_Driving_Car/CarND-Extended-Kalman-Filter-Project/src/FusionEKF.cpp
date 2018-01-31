6024ebd6523c382c6a94810803736e26febbf10e
gr8manbir <manbir.129@gmail.com>
2018-01-30 21:13:09 -0500

    Adding more checks

--- Self_Driving_Car/CarND-Extended-Kalman-Filter-Project/src/FusionEKF.cpp
+++ Self_Driving_Car/CarND-Extended-Kalman-Filter-Project/src/FusionEKF.cpp
@@ -144,20 +144,26 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
   previous_timestamp_ = measurement_pack.timestamp_;
+  
+  /* If dt is very small, we should not predict again as values won't change but it will mess
+     up calculations */
+  if( dt > 0.001)
+  {
 	
-  float dt_2 = dt * dt;
-  float dt_3 = dt_2 * dt;
-  float dt_4 = dt_3 * dt;
+	float dt_2 = dt * dt;
+	float dt_3 = dt_2 * dt;
+	float dt_4 = dt_3 * dt;
 	
-  ekf_.F_(0,2) = dt;
-  ekf_.F_(1,3) = dt;
+	ekf_.F_(0,2) = dt;
+	ekf_.F_(1,3) = dt;
 
-  ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
-	      0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
-	      dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
-	      0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
+	ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+			0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+			dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+			0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
 
-  ekf_.Predict();
+	ekf_.Predict();
+  }
 
   /*****************************************************************************
    *  Update
