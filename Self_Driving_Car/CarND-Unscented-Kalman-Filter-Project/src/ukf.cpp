0bbe24fa69d40f8e24226ffc0e35babe009ee78f
gr8manbir <manbir.129@gmail.com>
2018-02-15 23:01:54 -0500

    commenting out few cout

--- Self_Driving_Car/CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp
+++ Self_Driving_Car/CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp
@@ -176,8 +176,8 @@ void UKF::Prediction(double delta_t) {
    x_aug_(5) = 0;
    x_aug_(6) = 0;
    
-   std::cout<<"delta_t: "<<delta_t<<endl;
-   std::cout<<"Initial x_aug_=" <<x_aug_<<endl;
+   //std::cout<<"delta_t: "<<delta_t<<endl;
+   //std::cout<<"Initial x_aug_=" <<x_aug_<<endl;
    /* Fill p_aug with values */
    P_aug.topLeftCorner(5,5) = P_;
    P_aug(5,5) = std_a_*std_a_;
@@ -189,13 +189,13 @@ void UKF::Prediction(double delta_t) {
    
    /* Create augmented sigma points */
    Xsig_aug.col(0) = x_aug_;
-   std::cout<<"Sigma point aug: 0"<< " "<<Xsig_aug.col(0)<<endl;
+   //std::cout<<"Sigma point aug: 0"<< " "<<Xsig_aug.col(0)<<endl;
    for( int i = 0; i < n_aug_; i++ )
    {
        Xsig_aug.col(i+1)       = x_aug_ + sqrt(lambda_+n_aug_) * L.col(i);
        Xsig_aug.col(i+1+n_aug_) = x_aug_ - sqrt(lambda_+n_aug_) * L.col(i);
-       std::cout<<"Sigma point aug: "<<i+1<<" "<<Xsig_aug.col(i+1)<<endl;
-	   std::cout<<"Sigma point aug: "<<i+1+n_aug_<<" "<<Xsig_aug.col(i+1+n_aug_)<<endl;
+       //std::cout<<"Sigma point aug: "<<i+1<<" "<<Xsig_aug.col(i+1)<<endl;
+	   //std::cout<<"Sigma point aug: "<<i+1+n_aug_<<" "<<Xsig_aug.col(i+1+n_aug_)<<endl;
    }
      
    /* 2. Predict sigma points */
@@ -242,7 +242,7 @@ void UKF::Prediction(double delta_t) {
        Xsig_pred_(2,i) = v_p;   //1.73205
        Xsig_pred_(3,i) = yaw_p; //0
        Xsig_pred_(4,i) = yawd_p; //0
-	   std::cout<<"Sigma point prediction: "<<i<< " "<<Xsig_pred_.col(i)<<endl; 
+	   //std::cout<<"Sigma point prediction: "<<i<< " "<<Xsig_pred_.col(i)<<endl; 
    }
     /* 3. From sigma point predictions at time t+dt, calculate new mean and co-variance */
 
@@ -267,7 +267,7 @@ void UKF::Prediction(double delta_t) {
 
         P_ = P_ + weights_(i) * x_diff * x_diff.transpose() ;
     }
-	std::cout<<"Predicted P_"<<P_<<endl;
+	//std::cout<<"Predicted P_"<<P_<<endl;
 }
 
 /**
