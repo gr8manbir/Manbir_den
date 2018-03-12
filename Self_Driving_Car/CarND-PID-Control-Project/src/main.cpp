9ff4e50d417b3f7499c003dc7beab638f688130b
gr8manbir <manbir.129@gmail.com>
2018-03-11 20:32:59 -0400

    Fix compile issues

--- Self_Driving_Car/CarND-PID-Control-Project/src/main.cpp
+++ Self_Driving_Car/CarND-PID-Control-Project/src/main.cpp
@@ -37,7 +37,7 @@ int main()
   double init_Kp = 0.3;
   double init_Ki = 0;
   double init_Kd = 0.3;
-  pid.init(init_Kp, init_Ki, init_Kd);
+  pid.Init(init_Kp, init_Ki, init_Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
