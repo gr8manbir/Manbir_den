4306fe71e2e8c1f17d029573067205cfff2e8ebe
gr8manbir <manbir.129@gmail.com>
2018-03-13 20:38:21 -0400

    Adding comments

--- Self_Driving_Car/CarND-PID-Control-Project/src/PID.cpp
+++ Self_Driving_Car/CarND-PID-Control-Project/src/PID.cpp
@@ -15,6 +15,7 @@ void PID::Init(double Kp, double Ki, double Kd) {
 	this->Ki = Ki;
 	this->Kd = Kd;
 	
+	//Init all errors to zero
 	p_error = 0.0;
 	i_error = 0.0;
 	d_error = 0.0;
@@ -22,9 +23,12 @@ void PID::Init(double Kp, double Ki, double Kd) {
 }
 
 void PID::UpdateError(double cte) {
-	double previous_cte = p_error; // Note: p_error keeps track of cte in previous cycle 
+	double previous_cte = p_error; // Note: p_error keeps track of cte in previous cycle
+    // The proportional part of the error is the cross track error itself.	
 	p_error = cte;
+	// The integral part is to cumulatively add cte, so as to bring it closer to zero.
 	i_error += cte;
+	// The derivative part reduces current cte by the previous cte. This ensures that car smoothly transitions.
 	d_error = cte - previous_cte;
 }
 
